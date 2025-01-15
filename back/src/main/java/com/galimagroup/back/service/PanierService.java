f319eb45bcae1551f9541bfdf81343257d938d55
JeanNoelNdiaye <jeannoelndiaye1@gmail.com>
2025-01-15 09:21:28 +0000

    Suppression du dossier back

--- back/src/main/java/com/galimagroup/back/service/PanierService.java
+++
@@ -1,9 +0,0 @@
-package com.galimagroup.back.service;
-
-import com.galimagroup.back.entities.Panier;
-
-public interface PanierService {
-    public Panier getPanierByUser(String userEmail);
-    public Panier addProductToPanier(String userEmail, String productId);
-    public Panier removeProductFromPanier(String userEmail, String productId);
-}
